56c861e3a874d9449c81c8144587bd170acdb484
Vevaan <vevaan.s.verma@gmail.com>
2025-08-01 23:05:22 -0700

    Add server jar retrieval

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-import java.util.Scanner;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner console = new Scanner(System.in);
-        String command = console.nextLine();
+    }
 
-        Scanner lineReader = new Scanner(command);
+    // returns the path to the server jar file
+    private static String getServerJar(String client, String version) {
 
+        // url: https://mcutils.com/api/server-jars/{client}/{version}/download
+        String url = "https://mcutils.com/api/server-jars/" + client + "/" + version + "/download";
+        String jarName = client + "-" + version + ".jar";
+        String jarPath = "server-jars/" + jarName;
+
+        try {
+
+            Files.createDirectories(Paths.get("server-jars")); // make sure the directory exists
+            InputStream in = URI.create(url).toURL().openStream(); // open the input stream from the URL
+            Files.copy(in, Paths.get(jarPath), java.nio.file.StandardCopyOption.REPLACE_EXISTING); // copy the input stream to the file; replace if it exists already
+            return jarPath; // return the path to the jar file
+
+        } catch (Exception e) {
+
+            System.err.println("Error downloading server jar: " + e.getMessage()); // output error message
+            return null;
+
+        }
     }
 }
